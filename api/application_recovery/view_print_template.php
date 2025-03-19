d1b2b2ccd785222a2b38bd6a4b318897c7fceb9f
kimsungtaek1 <kstbook@naver.com>
2025-03-19 10:40:38 +0900

    자동 커밋: 전체 폴더 업데이트

--- api/application_recovery/view_print_template.php
+++ api/application_recovery/view_print_template.php
@@ -11,25 +11,29 @@
         </div>
         <div class="view-print-list">
 			<div class="checkbox-group flex-column">
-				<div class="view-print-item">
-					<input type="checkbox" id="" name="view_print[]" value="채권자목록 열람">
-					<label for="">채권자목록 열람</label>
+				<div class="view-print-select-all">
+					<input type="checkbox" id="select_all_items" name="select_all">
+					<label for="select_all_items">모두 선택</label>
 				</div>
 				<div class="view-print-item">
-					<input type="checkbox" id="" name="view_print[]" value="재산목록 열람">
-					<label for="">재산목록 열람</label>
+					<input type="checkbox" id="view_print_item_0" name="view_print[]" value="채권자목록 열람">
+					<label for="view_print_item_0">채권자목록 열람</label>
 				</div>
 				<div class="view-print-item">
-					<input type="checkbox" id="" name="view_print[]" value="수입지출목록 열람">
-					<label for="">수입지출목록 열람</label>
+					<input type="checkbox" id="view_print_item_1" name="view_print[]" value="재산목록 열람">
+					<label for="view_print_item_1">재산목록 열람</label>
 				</div>
 				<div class="view-print-item">
-					<input type="checkbox" id="" name="view_print[]" value="진술서 열람">
-					<label for="">진술서 열람</label>
+					<input type="checkbox" id="view_print_item_2" name="view_print[]" value="수입지출목록 열람">
+					<label for="view_print_item_2">수입지출목록 열람</label>
+				</div>
+				<div class="view-print-item">
+					<input type="checkbox" id="view_print_item_3" name="view_print[]" value="진술서 열람">
+					<label for="view_print_item_3">진술서 열람</label>
 				</div>
 			</div>
 			<div class="view-print-button">
-				<button type="button" class="btn-save btn-long" id="">열람/인쇄</button>
+				<button type="button" class="btn-save btn-long" id="view_print_btn">열람/인쇄</button>
 			</div>
         </div>
 		
