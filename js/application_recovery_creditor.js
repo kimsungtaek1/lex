2ec666188a6b0865b5d911a2f32bab1673c6eadf
kimsungtaek1 <kstbook@naver.com>
2025-03-27 10:17:29 +0900

    자동 커밋: 전체 폴더 업데이트

--- js/application_recovery_creditor.js
+++ js/application_recovery_creditor.js
@@ -1093,6 +1093,12 @@ $(document).ready(function() {
 			},
 			success: function(response) {
 				console.log(response);
+				if ($(`#undeterminedCount${count}`).length) {
+				  console.log(`요소 #undeterminedCount${count} 존재함`);
+				  $(`#undeterminedCount${count}`).text(response.count);
+				} else {
+				  console.log(`요소 #undeterminedCount${count} 없음`);
+				}
 				if (response.success) {
 					console.log('------'+response.count);
 					$(`#undeterminedClaimCount${count}`).text(response.count);
