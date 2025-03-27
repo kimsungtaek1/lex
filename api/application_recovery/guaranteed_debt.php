e108455b822970ab85e84ed894d6494f19abe9e3
kimsungtaek1 <kstbook@naver.com>
2025-03-27 12:31:10 +0900

    자동 커밋: 전체 폴더 업데이트

--- api/application_recovery/guaranteed_debt.php
+++ api/application_recovery/guaranteed_debt.php
@@ -83,7 +83,7 @@ if (!$debt_no && $case_no && $creditor_count) {
 					<option value="국가">국가</option>
 					<option value="지방자치단체">지방자치단체</option>
 				</select>
-				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				&nbsp;&nbsp;&nbsp;&nbsp;
 				<input type="text" id="financial_institution" placeholder="금융기관명" class="form-control">
 				<button type="button" class="btn btn-long btn-financial-institution" data-count="<?php echo $creditor_count; ?>">금융기관 검색</button>
 			</div>
