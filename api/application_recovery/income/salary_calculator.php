3d4aa747a32136fc1433ffc2592b21663c2c98f9
kimsungtaek1 <kstbook@naver.com>
2025-04-08 10:13:29 +0900

    2025-04-08

--- api/application_recovery/income/salary_calculator.php
+++ api/application_recovery/income/salary_calculator.php
@@ -12,7 +12,7 @@ if (empty($case_no)) {
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>월평균소득계산기</title>
-	<link rel="stylesheet" href="../css/salary_calculator.css">
+	<link rel="stylesheet" href="../../../css/salary_calculator.css">
 	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
